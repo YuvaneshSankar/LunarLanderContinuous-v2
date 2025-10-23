18f84a27bc7f9098a21e335ebc7e3178b3ea533a
Yuvanesh Sankar <yuvanesh.skv@gmail.com>
2025-10-23 14:45:11 +0530

    refractor(lunar_lader_env.py):added the clip action for valid range

--- envs/lunar_lander_env.py
+++ envs/lunar_lander_env.py
@@ -1,27 +1,26 @@
 import gym
+import numpy as np
 
-class LunarLanderEnvWrapper:
+class LunarLanderEnv:
     def __init__(self):
-        self.env = gym.make('LunarLanderContinuous-v2')
-        self.env._max_episode_steps = 1000
+        self.env = gym.make("LunarLanderContinuous-v2")
+        self.state_dim = self.env.observation_space.shape[0]
+        self.action_dim = self.env.action_space.shape[0]
+        self.action_low = self.env.action_space.low
+        self.action_high = self.env.action_space.high
 
     def reset(self):
-        return self.env.reset()
+        state = self.env.reset()
+        return np.array(state, dtype=np.float32)
 
     def step(self, action):
+        # Clip action to valid range
+        action = np.clip(action, self.action_low, self.action_high)
         next_state, reward, done, info = self.env.step(action)
-        return next_state, reward, done, info
+        return np.array(next_state, dtype=np.float32), reward, done, info
 
     def render(self):
         self.env.render()
 
     def close(self):
         self.env.close()
-
-    @property
-    def action_space(self):
-        return self.env.action_space
-
-    @property
-    def observation_space(self):
-        return self.env.observation_space
